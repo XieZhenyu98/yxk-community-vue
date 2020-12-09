e29469592c4ff91d3c09ea5a8eb477f97a507551
xiezhenyu98 <751811152@qq.com>
2020-12-09 18:15:20 +0800

    feat:添加element.js按需导入文件

---
+++ src/plugins/element.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+
+import {
+  Button, Main, Container, Footer, Header, Row, Col, Card, Table, TableColumn, Tag, Menu, Submenu, MenuItem, Calendar,
+  Link, Form, FormItem, Input, Message, Radio, RadioGroup, Select, Option, OptionGroup, Tooltip, Loading
+} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Main)
+Vue.use(Container)
+Vue.use(Footer)
+Vue.use(Header)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Card)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Tag)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Calendar)
+Vue.use(Link)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(OptionGroup)
+Vue.use(Tooltip)
+Vue.use(Loading)
+Vue.prototype.$message = Message
